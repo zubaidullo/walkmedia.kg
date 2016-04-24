e62aafaa2fc9cd97cf00c5bfce8fc7b6afda85e7
Zubaidullo Niimatullo uulu <zniimatullo@critical-factor.com>
2016-04-25 00:13:06 +0600

    Add partner reviews

--- src/main/java/conf/Routes.java
+++ src/main/java/conf/Routes.java
@@ -28,9 +28,7 @@ public class Routes implements ApplicationRoutes {
     public void init(Router router) {  
         
         router.GET().route("/").with(ApplicationController.class, "index");
-        router.GET().route("/hello_world.json").with(ApplicationController.class, "helloWorldJson");
-        
- 
+
         ///////////////////////////////////////////////////////////////////////
         // Assets (pictures / javascript)
         ///////////////////////////////////////////////////////////////////////    
